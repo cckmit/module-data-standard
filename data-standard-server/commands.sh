92ab5372de5acc112e668f1bc23dc1dd95623199
a-chenhonglei <267326633@qq.com>
2022-05-09 18:50:48 +0800

    解决不能正常依赖发版

--- data-standard-server/commands.sh
+++ data-standard-server/commands.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /app
 /bin/cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo 'Asia/Shanghai' >/etc/timezone
-java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=80 -XX:+PrintClassHistogram -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -XX:+HeapDumpOnOutOfMemoryError -Xloggc:/app/logs/gc.log -jar -Dspring.profiles.active=$DEFOR /app/dnt-warehouse-server-1.0.jar $* &
+java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=80 -XX:+PrintClassHistogram -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -XX:+HeapDumpOnOutOfMemoryError -Xloggc:/app/logs/gc.log -jar -Dspring.profiles.active=$DEFOR /app/data-standard-server-1.0.jar $* &
 while true; do sleep 1; done
